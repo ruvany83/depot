d3b23e1573adde1fff381ddf4db438cc16d76dfe
yvan <ruvany@gmail.com>
2018-11-05 20:44:23 +0200

    Cart Creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
-  def index
-  	@products = Product.order(:title)
-  end
+	include ActionView::Helpers::TextHelper
+before_action :increment_counter, only: [:index]
+
+    def index
+        @products = Product.order(:title)
+    end
+
+    private
+        def increment_counter
+            if session[:counter].nil?
+                session[:counter] = 0
+            end
+            session[:counter] += 1
+            @counter = session[:counter]
+            @counter_message = "You have visited this page #{pluralize(@counter, "time")}" if @counter > 65
+        end  
 end
